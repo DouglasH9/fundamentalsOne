56155bff54124ed3ede56f111a8a25e96ab0f9b5
Doug Herman <douglash9882@gmail.com>
2021-09-27 12:57:31 -0500

    csharp fund one

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace fundamentalsOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //     for (var i = 1; i <= 255; i++)
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            // for (int i = 1; i <= 100; i++)
+            // {
+            //     if (i % 15 != 0 && (i % 3 == 0 || i % 5 == 0))
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
